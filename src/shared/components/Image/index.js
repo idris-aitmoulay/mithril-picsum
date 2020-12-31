f4a98b3be018177c70897b16670e452dc79f9cf3
Idris AIT MOUALY <idris.aitmoulay@gmail.com>
2020-12-31 22:14:04 +0100

    Panel Done,

--- src/shared/components/Image/index.js
+++ src/shared/components/Image/index.js
@@ -3,14 +3,28 @@ import _ from "lodash";
 
 const Image = () => {
   let src = '', alt = '';
+  let onMouseEnter, onMouseLeave;
   const oninit = ({ attrs }) => {
     src = _.get(attrs, 'src', 'https://developer.mozilla.org/static/img/favicon144.png');
     alt = _.get(attrs, 'alt', 'Picture');
+    onMouseEnter = _.get(attrs, 'onMouseEnter', undefined);
+    onMouseLeave = _.get(attrs, 'onMouseLeave', undefined);
   };
 
+  const onmouseenter = () => {
+    if (onMouseEnter) onMouseEnter();
+  };
+
+  const onmouseleave = () => {
+    if (onMouseLeave) onMouseLeave();
+  };
   return {
     oninit,
-    view: () => <img src={src} alt={alt} />
+    view: () => <img
+      src={src}
+      alt={alt}
+      onmouseenter={onmouseenter}
+      onmouseleave={onmouseleave}/>
   };
 };
 
--- src/shared/components/Panel/index.js
+++ src/shared/components/Panel/index.js
@@ -5,18 +5,24 @@ import './styles.scss';
 
 
 const Panel = () => {
-  let src = '', text = '';
+  let src = '', text = '', hidden = true;
   const oninit = ({ attrs }) => {
     text = _.get(attrs, 'text', 'live is');
     src = _.get(attrs, 'src', undefined);
+  };
+  const onMouseEnter = () => {
+    hidden = false;
+  };
+  const onMouseLeave = () => {
+    hidden = true;
   };
 
   return {
     oninit,
     view: () => (
       <div class="panel">
-        <Image src={src} />
-        <div class="panel_text">
+        <Image src={src} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave}/>
+        <div hidden={hidden} class="panel_text" onmouseenter={onMouseEnter}>
           <Typography variant={"h1"}>{text}</Typography>
         </div>
       </div>
